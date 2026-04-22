2211ee0981648b7033a27e88056d5dac7cdad3d1
JmoaOCC <jmoa.occ.ses@gmail.com>
2026-04-22 11:44:54 +0200

    Update supabase.js

--- js/services/supabase.js
+++ js/services/supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL='TU_URL';
-const SUPABASE_ANON_KEY='TU_KEY';
+const SUPABASE_URL='https://wxvgxcogrlcflxldaemw.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY='sb_publishable_MxUoL5CYRj9LP4Kiuueg9A_tYkA69gq';
 export const supabase=window.supabase.createClient(
 SUPABASE_URL,SUPABASE_ANON_KEY
 );
